8a7fd7702568b532362fc8af46d64192fd7768b9
Maheshwickramage <139673873+Maheshwickramage@users.noreply.github.com>
2024-02-15 22:36:31 +0530

    Make_athuntivation_clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
-import {Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
-subsets: ["latin"],
-weight : ["400", "500", "600", "700"],
-variable:'--font-poppins',
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
   title: "Evently",
   description: "Evently is a platform for creating and managing events.",
-  icons:
-  {icon:"/assets/images/logo.svg"}
+  icons: { icon: "/assets/images/logo.svg" },
 };
 
 export default function RootLayout({
@@ -21,8 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Header from "@/components/shared/Header";
 
 export default function Home() {
   return (
 
     <main>
 
-      <h1 className="text-4xl">Evently</h1>
+      <h1 className="text-4xl"></h1>
+      <Header/>
       
     </main>
 
