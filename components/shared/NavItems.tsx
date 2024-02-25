2469ac95e918c11bd48c820eaf46768896001f1e
Maheshwickramage <139673873+Maheshwickramage@users.noreply.github.com>
2024-02-25 23:47:29 +0530

    Upadate_NavBar

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -4,10 +4,13 @@ import React from 'react'
 
 
 const NavItems = () => {
+
+  const pathname =usePathname();
   return (
     <ul className='md:flex-between flex w-full flex-col items-start gap-5 md:flex-row'>
     
     {headerLinks.map((link)=>{
+      const isActive = pathname === link.route;
 
       return(
         <li>
